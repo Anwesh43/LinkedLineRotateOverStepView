d9a0e826fe1e9fb11e5e316cbea2c5b971ce2623
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 02:18:49 +0530

    adding libraries and defining constants

--- linerotateoverstepview/src/main/java/com/anwesh/uiprojects/linerotateoverstepview/LineRotateOverStepView.kt
+++ linerotateoverstepview/src/main/java/com/anwesh/uiprojects/linerotateoverstepview/LineRotateOverStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linerotateoverstepview
 /**
  * Created by anweshmishra on 27/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val color : Int = Color.parseColor("#2ecc71")
+val sizeFactor : Float = 2.6f
+val strokeFactor : Int = 90
