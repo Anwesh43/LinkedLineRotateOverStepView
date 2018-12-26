473b99b2682e6a278a7228c28f277fae30f73a94
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 02:44:48 +0530

    adding LineRotateOverStepView class

--- linerotateoverstepview/src/main/java/com/anwesh/uiprojects/linerotateoverstepview/LineRotateOverStepView.kt
+++ linerotateoverstepview/src/main/java/com/anwesh/uiprojects/linerotateoverstepview/LineRotateOverStepView.kt
@@ -63,3 +63,21 @@ fun Canvas.drawLRONode(i : Int, scale : Float, paint : Paint) {
     restore()
     restore()
 }
+
+class LineRotateOverStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
