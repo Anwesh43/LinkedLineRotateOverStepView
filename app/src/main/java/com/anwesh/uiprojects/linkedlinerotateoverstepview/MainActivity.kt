c9e664457ae87667a7f4c61306e5ad428a06a361
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 10:28:29 +0530

    adding view in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinerotateoverstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinerotateoverstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinerotateoverstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linerotateoverstepview.LineRotateOverStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineRotateOverStepView.create(this)
     }
 }
