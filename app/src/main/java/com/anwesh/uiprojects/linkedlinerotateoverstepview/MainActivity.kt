b42ef33d9c55ceeeaa7e2047a997fedbc3e21bcb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 13:03:25 +0530

    adding AnimatonListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinerotateoverstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinerotateoverstepview/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anwesh.uiprojects.linkedlinerotateoverstepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linerotateoverstepview.LineRotateOverStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotateOverStepView.create(this)
+        val view : LineRotateOverStepView = LineRotateOverStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({
+            showToast("animation number $it is completed")
+        }, {
+            showToast("animation number $it is reset")
+        })
+    }
+
+    fun showToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
